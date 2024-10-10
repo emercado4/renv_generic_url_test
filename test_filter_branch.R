f54138976271d8d421ae98f19a60230fc7fb343c
mercae <Elisha.Mercado@ons.gov.uk>
2024-10-10 11:47:32 +0100

    commit another csv

--- test_filter_branch.R
+++ test_filter_branch.R
@@ -4,4 +4,8 @@ library(dplyr)
 data <- tibble(a = seq(1,10000, 1), b = a/2, c = (b-1)*3)
 
 
-write.csv(data, "test_file.csv", row.names = FALSE)
+write.csv(data, "./renv_generic_url_test/test_file.csv", row.names = FALSE)
+
+# write another csv - iris data
+
+write.csv(iris, "./renv_generic_url_test/iris.csv", row.names = FALSE)
